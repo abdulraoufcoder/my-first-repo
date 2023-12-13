c003146cb0104aace909245e9ce49fc1abbe2bf3
Abdul Raouf <raoufiscoder1033@gmail.com>
2023-12-13 19:15:51 +0500

    ng for

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HeaderComponent } from './header/header.component';
 import { StudentModule } from './student/student.module';
 import { TeacherModule } from './teacher/teacher.module';
 import { IfElseComponent } from './if-else/if-else.component';
+import { NgForComponent } from './ng-for/ng-for.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     IfElseComponent,
+    NgForComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ng-for/ng-for.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-for',
+  templateUrl: './ng-for.component.html',
+  styleUrls: ['./ng-for.component.scss']
+})
+export class NgForComponent {
+
+  nums: any = [
+    1, 2, 3, 4, 5, 6
+  ]
+
+  pageNumber: number = 3;
+  pageSize: number = 10;
+  names: string[] = [
+    'zargham', 'ahmed', 'surraya', 'hamza', 'talha', 'aleesha', 'asad', 'ali', 'amna', 'areesha'
+  ]
+
+  nestedArr = [
+    {
+      name: 'Books',
+      items: [
+        "rich dad poor dad",
+        "how to win friends and influence people",
+        "atomic habits",
+        "ego is the enemy"
+      ]
+    },
+    {
+      name: 'Seasons',
+      items: [
+        "spring",
+        "winter",
+        "autumn",
+        "summer"
+      ]
+    },
+    {
+      name: 'Tea Types',
+      items: [
+        "green tea",
+        "black tea",
+        "capachino",
+        "latte"
+      ]
+    },
+
+
+  ]
+
+  printArray() {
+    console.log(this.nums);
+  }
+}
