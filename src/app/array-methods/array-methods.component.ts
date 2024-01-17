57ecfd5843118ce9ecacfe5d8044795107766050
Abdul Raouf Coder <raoufiscoder1033@gmail.com>
2024-01-17 08:44:56 +0500

    array methods day 2

--- src/app/array-methods/array-methods.component.ts
+++ src/app/array-methods/array-methods.component.ts
@@ -14,6 +14,23 @@ export class ArrayMethodsComponent implements OnInit{
   arrayMethods!: {title: string, value: string | number};
 
    arr = [1,2,3,4,5,6,7,8,9,10];
+
+   kids: any[] = [
+    {
+      name: "Ali",
+      age: 16
+    },
+    {
+      name: "Ramzan",
+      age: 30
+    },
+    {
+      name: "Asad",
+      age: 21
+    },
+
+   ]
+
    names = ['ali', 'asad']
    variable:any;
    newArr: any[] = [2];
@@ -26,6 +43,23 @@ export class ArrayMethodsComponent implements OnInit{
     this.variable = this.names.join(' ');
     console.log(typeof this.variable);
     
+
+    let findKids = this.kids.find(item => item.age == 21)
+
+    let findKidsIndex = this.kids.findIndex((item)=>{
+      return item.age == 21
+    })
+
+
+
+    console.log(findKids);
+    console.log("Index", findKidsIndex)
+    
+    
+   }
+
+   funct(){
+    // console.log(findKidsIndex);
     
    }
 }
